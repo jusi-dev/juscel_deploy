12a91c767908c3c5f2c364815429bfb7b62ce25d
Justin Winistoerfer <winistoerfer.justin@gmx.ch>
2024-03-06 10:37:46 +0100

    Run poll continously

--- src/index.ts
+++ src/index.ts
@@ -28,39 +28,46 @@ const receiveMessage = (queueUrl : string) =>
 
 export const main = async (queueUrl = SQS_QUEUE_URL) => {
     while (true) {
-        const { Messages } = await receiveMessage(queueUrl);
+        try {
+            const { Messages } = await receiveMessage(queueUrl);
 
-        if (!Messages) {
-            return;
-        }
+            if (!Messages) {
+                await new Promise((resolve) => setTimeout(resolve, 5000));
+                continue;
+            }
 
-        if (Messages.length === 1) {
-            console.log(Messages[0].Body);
-            await client.send(
-            new DeleteMessageCommand({
-                QueueUrl: queueUrl,
-                ReceiptHandle: Messages[0].ReceiptHandle,
-            }),
-            );
+            if (Messages.length === 1) {
+                console.log(Messages[0].Body);
+                await client.send(
+                new DeleteMessageCommand({
+                    QueueUrl: queueUrl,
+                    ReceiptHandle: Messages[0].ReceiptHandle,
+                }),
+                );
 
-            const id = Messages[0].Body;
-            updateStatus(id ?? "", "Building Project...");
-            await downloadS3Folder(`output/${id}`)
-            await buildProject(id ?? "");
-            await copyFinalDist(id ?? "");
-            updateStatus(id ?? "", "deployed");
-            await removeOutputs(id ?? "");
+                const id = Messages[0].Body;
+                updateStatus(id ?? "", "Building Project...");
+                await downloadS3Folder(`output/${id}`)
+                await buildProject(id ?? "");
+                await copyFinalDist(id ?? "");
+                updateStatus(id ?? "", "deployed");
+                await removeOutputs(id ?? "");
 
-        } else {
-            await client.send(
-            new DeleteMessageBatchCommand({
-                QueueUrl: queueUrl,
-                Entries: Messages.map((message) => ({
-                Id: message.MessageId,
-                ReceiptHandle: message.ReceiptHandle,
-                })),
-            }),
-            );
+            } else {
+                await client.send(
+                new DeleteMessageBatchCommand({
+                    QueueUrl: queueUrl,
+                    Entries: Messages.map((message) => ({
+                    Id: message.MessageId,
+                    ReceiptHandle: message.ReceiptHandle,
+                    })),
+                }),
+                );
+            }
+            
+        } catch (err) {
+            console.error('An error occurred:', err);
+            await new Promise(resolve => setTimeout(resolve, 5000));
         }
     }
 };
